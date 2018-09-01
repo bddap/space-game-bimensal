2315429515261f314c4870a88c2a283dc91a5603
Andrew Dirksen <andrew@dirksen.com>
2018-08-31 18:20:11 -0700

    input map

--- src/human.rs
+++ src/human.rs
@@ -5,10 +5,22 @@ impl Human {
     pub fn new() -> Human {
         Human {}
     }
-    pub fn display(&self, world: &::world::World) {
+
+    pub fn display(&self, _world: &::world::World) {
         println!("hello world");
     }
+
     pub fn input(&self) -> ::input::Input {
-        ::input::Input::Quit
+        let key = 'q';
+        match key {
+            'd' => ::input::Input::Move(::direction::Direction::North),
+            'a' => ::input::Input::Move(::direction::Direction::South),
+            'f' => ::input::Input::Move(::direction::Direction::East),
+            's' => ::input::Input::Move(::direction::Direction::West),
+            'j' => ::input::Input::Move(::direction::Direction::Up),
+            'k' => ::input::Input::Move(::direction::Direction::Down),
+            'q' => ::input::Input::Quit,
+            _ => ::input::Input::None,
+        }
     }
 }
